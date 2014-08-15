b0061ec7f3120e7173be1461c77f4654ceab192a
Dean Hillan <dean.hillan@gmail.com>
2014-08-14 23:25:45 -0400

    added more 256 data

--- get_images_async.py
+++ get_images_async.py
@@ -1,3 +1,4 @@
+
 import asyncio
 import aiohttp
 import math
@@ -8,7 +9,7 @@ def generate_url_list(base_url, year):
   url_list = []
   file_list = []
   year_str = str(year)
-  for month in range(5,13):
+  for month in range(1,13):
     month_str = str(month)
     if month<10:
       month_str = '0'+ month_str
@@ -27,13 +28,14 @@ def generate_url_list(base_url, year):
           file_str = year_str + month_str + day_str + '_' + hour_str + minute_str + '_M_256.jpg'
           file_list.append(file_str)
           url_list.append(base_url + year_str + '/' + month_str + '/' + day_str + '/' +  file_str)
-  return url_list
+  return url_list, file_list
 
 @asyncio.coroutine
-def save_to_file(img, url):
-  # file_name = file_list[idx]
-  file_name = list(url)
-  file_name = "".join((file_name[-25:len(file_name)]))
+def save_to_file(img, file_name):
+  # file_idx = idx + 10;
+  # file_name = file_list[file_idx]
+  print(type(file_name))
+  print("printing file name: " + file_name)
   f = open('./data/2010_256/' + file_name,'wb')
   f.write(img)
   f.close()
@@ -43,7 +45,7 @@ def fetch(url):
   data = ""
   try:
     yield from asyncio.sleep(1)
-    response = yield from aiohttp.request('GET', url, connector=connector)
+    response = yield from aiohttp.request('GET', url)
   except Exception as exc:
       print("ERROR ", url, 'has error', repr(str(exc)))
   else:
@@ -54,43 +56,38 @@ def fetch(url):
   return data
 
 @asyncio.coroutine
-def fetch_image(url, idx, file_list):
-    print("fetching image with idx %d", idx)
-    page = yield from fetch(url)
-    print("got image with idx %d", idx)
+def fetch_image(url, file_name):
+    img = yield from fetch(url)
 
     # save image to file
-    yield from save_to_file(page, url)
+    yield from save_to_file(img, file_name)
 
 @asyncio.coroutine
-def process_batch_of_urls(round, urls, file_list):
-  print("Batch %d starting" % round)
+def process_batch_of_urls(urls, file_names):
   coros = []
-  for idx, url in enumerate(urls):
-    coros.append(asyncio.Task(fetch_image(url, idx)))
+  for url, file_name in zip(urls, file_names):
+    coros.append(asyncio.Task(fetch_image(url, file_name)))
 
   yield from asyncio.gather(*coros)
-  print("Round %d finished" % round)
 
 @asyncio.coroutine
 def process_all():
   print('### Started ###')
   start_time = time.time()
-  file_list = generate_url_list('http://jsoc.stanford.edu/data/hmi/images/', 2010)
+  url_list, file_list = generate_url_list('http://jsoc.stanford.edu/data/hmi/images/', 2011)
 
-  chunks=[file_list[x:x+100] for x in range(0, len(file_list), 100)]
-  print("Total number of batches %d" % len(chunks))
+  url_chunks=[url_list[x:x+100] for x in range(0, len(url_list), 100)]
+  file_chunks=[file_list[x:x+100] for x in range(0, len(file_list), 100)]
 
-
-  for idx, batch in enumerate(chunks):
-    yield from process_batch_of_urls(idx, batch, chunks)
+  for urls, file_names in zip(url_chunks, file_chunks):
+    yield from process_batch_of_urls(urls, file_names)
 
   total_time = time.time() - start_time
-  print("Total number of api requests %d" % len(urls))
+  print("Total number of api requests %d" % len(url_list))
   print("Total time taken to process api requests is %s seconds" % total_time)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     # connector stores cookies between requests and uses connection pool
-    connector = aiohttp.TCPConnector(share_cookies=True, loop=loop)
+    # connector = aiohttp.TCPConnector(share_cookies=True, loop=loop)
     loop.run_until_complete(process_all())
